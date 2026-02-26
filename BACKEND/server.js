4912068ec88266d55e8ca5b07b8bfff2595effe5
Franco Acevedo <francoeacevedo37@gmail.com>
2026-02-26 19:28:55 -0300

    instalacion de dependencias y creacion de db y conexion con mongodb

---
+++ BACKEND/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor en puerto ${PORT}`);
+});
